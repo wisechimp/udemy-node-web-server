0f7ea9c1705e9236c392bb2c68c1b3ce66996f0f
shicks1080 <stuart.hicks@gmail.com>
2019-11-25 13:28:12 +0100

    Swapping handlebars templates in rather than static html files.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,29 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Bender Rodriguez'
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'Round about now',
+    name: 'The funk soul brother.'
+  })
+})
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Pleeese!',
+    message: 'I need somebody'
+  })
+})
 
 app.get('/weather', (req, res) => {
   res.send({
