559e18f7983097f6c137926a7a3cb624d27cb679
shicks1080 <stuart.hicks@gmail.com>
2019-11-26 13:50:58 +0100

    Adding a form
    And wiring it up to print data to terminal.

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,22 @@
 console.log('Client side javascript file is loaded.')
 
-fetch('http://localhost:3000/weather?address=staffanstorp')
-  .then((response) => {
-    response.json()
-      .then((data) => {
-        if (data.error) {
-          return console.log(data.error)
-        }
-        console.log(data.location)
-        console.log(data.forecast)
-      })
-  })
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const location = search.value
+
+  fetch('http://localhost:3000/weather?address=' + location)
+    .then((response) => {
+      response.json()
+        .then((data) => {
+          if (data.error) {
+            return console.log(data.error)
+          }
+          console.log(data.location)
+          console.log(data.forecast)
+        })
+    })
+} )
