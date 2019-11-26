a96d969b1c880606ba0f050451946581d777db28
shicks1080 <stuart.hicks@gmail.com>
2019-11-26 13:38:12 +0100

    Adding fetch
    Adding the ability to pull a forecast from the webpage.

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side javascript file is loaded.')
+
+fetch('http://localhost:3000/weather?address=staffanstorp')
+  .then((response) => {
+    response.json()
+      .then((data) => {
+        if (data.error) {
+          return console.log(data.error)
+        }
+        console.log(data.location)
+        console.log(data.forecast)
+      })
+  })
