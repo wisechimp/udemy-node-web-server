36b0826fb80f33b59a332650421868c9100b03d9
shicks1080 <stuart.hicks@gmail.com>
2019-11-26 14:05:04 +0100

    Adding the forecast to the page
    Wiring up the hbs page to present the data when it's collected.
    Styling.

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,29 @@ console.log('Client side javascript file is loaded.')
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = ''
+messageTwo.textContent = ''
 
 weatherForm.addEventListener('submit', (e) => {
   e.preventDefault()
 
   const location = search.value
 
+  messageOne.textContent = 'loading...'
+  messageTwo.textContent = ''
+
   fetch('http://localhost:3000/weather?address=' + location)
     .then((response) => {
       response.json()
         .then((data) => {
           if (data.error) {
-            return console.log(data.error)
+            return messageOne.textContent = data.error
           }
-          console.log(data.location)
-          console.log(data.forecast)
+          messageOne.textContent = data.location
+          messageTwo.textContent = data.forecast
         })
     })
-} )
+})
